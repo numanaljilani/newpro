259f14ad0838768d53c292389dd1b14a12512ea8
numanaljilani <numanaljilani001@gmail.com>
2024-07-26 20:44:15 +0530

    new chat flask

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './templates/**/*.html', // Adjust the path to your Jinja template files
+      './templates/*.html', // Adjust the path to your Jinja template files
+      './static/js/**/*.js',   // Adjust the path to your JavaScript files if needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
